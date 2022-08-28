6b5f654cf48bee2fcf8bbcd9e888371c809a5f14
Furkan Gundogan <furkan.gundogan@stu.fsm.edu.tr>
2022-08-28 17:21:20 +0300

    create-native-app

---
+++ TelegramClone/App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+} from 'react-native';
+
+const App = () => {
+
+  return (
+    <SafeAreaView>
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic">
+      </ScrollView>
+    </SafeAreaView>
+  );
+};
+
+export default App;
