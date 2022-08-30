d4c17c33f4c0581adf6e95ce72fb254052550ad4
Furkan Gundogan <furkan.gundogan@stu.fsm.edu.tr>
2022-08-30 17:44:08 +0300

    Picker init

--- TelegramClone/src/components/LoginForm.js
+++ TelegramClone/src/components/LoginForm.js
@@ -1,36 +1,71 @@
 import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 import React, {useContext, useState} from 'react';
-import { UserContext } from '../contexts/userContext';
-
+import {UserContext} from '../contexts/userContext';
+import {Picker} from '@react-native-picker/picker';
 const LoginForm = () => {
-  const {user,setUser} = useContext(UserContext);
-    const initialState={
-        firstname:"",
-        lastname:"",
-        phone:"",
-        username:""
-    }
+  const {user, setUser} = useContext(UserContext);
+  const initialState = {
+    firstname: '',
+    lastname: '',
+    country: '+90',
+    phone: '',
+    username: '',
+  };
   const [userInfo, setuserInfo] = useState(initialState);
-  const handleChange=(e,name)=>{
-    setuserInfo(prevState=>({
-        ...prevState,
-        [name]:e
-    }))
-    console.log(userInfo)
-  }
+  const handleChange = (e, name) => {
+    setuserInfo(prevState => ({
+      ...prevState,
+      [name]: e,
+    }));
+    console.log(userInfo);
+  };
   const submitUser = () => {
-    setUser(userInfo)
-    console.log("user set completed:",userInfo)
-    console.log("user:",user);
-  }
+    setUser(userInfo);
+    console.log('user set completed:', userInfo);
+    console.log('user:', user);
+  };
+
   return (
     <View>
       <Text>LoginPage</Text>
-      <TextInput style={styles.textInput} onChangeText={(e)=>handleChange(e,"firstname")}/>
-      <TextInput style={styles.textInput} onChangeText={(e)=>handleChange(e,"lastname")}/>
-      <TextInput style={styles.textInput} onChangeText={(e)=>handleChange(e,"phone")}/>
-      <TextInput style={styles.textInput} onChangeText={(e)=>handleChange(e,"username")}/>
-      <Button title="Login" onPress={submitUser}/>
+      <View style={styles.phoneWrapper}>
+        <View  style={styles.countryPicker}>
+        <Picker
+         
+          selectedValue={userInfo?.country}
+          onValueChange={(itemValue, itemIndex) =>
+            handleChange(itemValue, 'country')
+          }>
+          <Picker.Item style={styles.pickerItem} label="+90" value="+90" />
+          <Picker.Item style={styles.pickerItem} label="+60" value="+60" />
+        </Picker>
+        </View>
+        <View style={styles.phoneInput}>
+          <TextInput
+            style={styles.textInput}
+            placeholder="Phone"
+            onChangeText={e => handleChange(e, 'phone')}
+          />
+        </View>
+      </View>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Firstname"
+        onChangeText={e => handleChange(e, 'firstname')}
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder="Lastname"
+        onChangeText={e => handleChange(e, 'lastname')}
+      />
+
+      <TextInput
+        style={styles.textInput}
+        placeholder="Username"
+        onChangeText={e => handleChange(e, 'username')}
+      />
+
+      <Button title="Login" onPress={submitUser} />
     </View>
   );
 };
@@ -38,8 +73,33 @@ const LoginForm = () => {
 export default LoginForm;
 
 const styles = StyleSheet.create({
-    textInput:{
-        borderWidth:1,
-        borderColor:"black"
-    }
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'black',
+    fontSize: 18,
+    borderRadius: 8,
+    marginLeft: 32,
+    marginRight: 32,
+    marginBottom: 16,
+    height: 40,
+    padding: 8,
+  },
+  phoneWrapper: {
+    flexDirection: 'row',
+  },
+  phoneInput: {
+    flex: 2,
+  },
+  countryPicker: {
+    flex: 1,
+    height: 40,
+    paddingLeft:16,
+    alignContent:"center",
+    marginTop:-4,
+    marginRight:-32,
+    
+  },
+  pickerItem: {
+    fontSize: 18,
+  },
 });
