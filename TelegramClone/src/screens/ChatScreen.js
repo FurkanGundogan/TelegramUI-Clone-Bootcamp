29bda2e03c654db63d5283436b226e998f082327
Furkan Gundogan <furkan.gundogan@stu.fsm.edu.tr>
2022-09-02 20:48:50 +0300

    Chat Screen Header design

--- TelegramClone/src/screens/ChatScreen.js
+++ TelegramClone/src/screens/ChatScreen.js
@@ -1,12 +1,18 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import useTheme from '../hooks/useTheme'
+import { useRoute } from '@react-navigation/native'
+import Header from '../components/ChatHeader'
 
 const ChatScreen = () => {
   const {theme}=useTheme()
+  const route = useRoute()
+  const {contact} = route?.params;
   return (
     <View style={{...styles.container,backgroundColor:theme?.backgroundColor}}>
-      <Text>ChatScreen</Text>
+      <Header contact={contact} />
+      <MessageArea/>
+     
     </View>
   )
 }
@@ -16,6 +22,7 @@ export default ChatScreen
 const styles = StyleSheet.create({
   container:{
     backgroundColor:"white",
-        flex:1
+        flex:1,
+        paddingBottom: Platform.OS === 'android' ? 58 : 0,
   }
 })
